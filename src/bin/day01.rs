d458ad1959dcf9ce2857a8d90a74fdc24099fd85
Max McCarthy <max_mccarthy@apple.com>
2020-11-25 18:06:01 -0800

    Add solution runner and example day01 from last year

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,3 +1,67 @@
-fn main() {
-    println!("Hello, world!");
+// Note: this is day 1 from 2019, not 2020
+
+use aoc::{self, Result, Solve};
+use std::{num::ParseIntError, str::FromStr};
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+struct Masses(Vec<i32>);
+
+impl FromStr for Masses {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let masses = s.lines().map(str::parse).collect::<Result<_, _>>()?;
+        Ok(Masses(masses))
+    }
+}
+
+struct PartOne;
+
+impl PartOne {
+    fn module_fuel(module: &i32) -> i32 {
+        module / 3 - 2
+    }
+}
+
+impl Solve for PartOne {
+    type Input = Masses;
+    type Solution = i32;
+
+    fn solve(input: &Masses) -> Result<Self::Solution> {
+        Ok(input.0.iter().map(Self::module_fuel).sum())
+    }
+}
+
+struct PartTwo;
+
+impl PartTwo {
+    fn total_module_fuel(module: &i32) -> i32 {
+        let init = Some(PartOne::module_fuel(module));
+        std::iter::successors(init, |m| Some(PartOne::module_fuel(m)))
+            .take_while(|&m| m > 0)
+            .sum()
+    }
+}
+
+impl Solve for PartTwo {
+    type Input = Masses;
+    type Solution = i32;
+
+    fn solve(input: &Masses) -> Result<Self::Solution> {
+        Ok(input.0.iter().map(Self::total_module_fuel).sum())
+    }
+}
+
+aoc::main!();
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn regressions() {
+        let input = aoc::input!();
+        assert_eq!(PartOne::solve(&input).unwrap(), 3262991);
+        assert_eq!(PartTwo::solve(&input).unwrap(), 4891620);
+    }
 }
---
+++ src/lib.rs
@@ -0,0 +1,73 @@
+use anyhow::{self, Context};
+use std::{error::Error, fmt::Display, path::Path, str::FromStr};
+
+pub use anyhow::Result;
+
+pub trait Solve {
+    type Input: ?Sized;
+    type Solution: Display;
+
+    fn solve(input: &Self::Input) -> Result<Self::Solution>;
+}
+
+#[macro_export]
+macro_rules! main {
+    () => {
+        fn main() -> $crate::Result<()> {
+            let path = $crate::input_path!();
+            $crate::_main::<_, PartOne, PartTwo>(&path)
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! input_path {
+    () => {{
+        use std::path::{Path, PathBuf};
+        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path.push("input/");
+        path.push(Path::new(file!()).file_stem().expect("missing file stem"));
+        path.set_extension("txt");
+        path
+    }};
+}
+
+#[macro_export]
+macro_rules! input {
+    () => {{
+        let path = $crate::input_path!();
+        $crate::_input(&path).expect("failed to get input")
+    }};
+}
+
+pub fn _input<P, I>(path: P) -> Result<I>
+where
+    P: AsRef<Path>,
+    I: FromStr,
+    <I as FromStr>::Err: Error + Send + Sync + 'static,
+{
+    let path = path.as_ref();
+    let text = std::fs::read_to_string(path)
+        .with_context(|| format!("failed to read input file:\n{}", path.display()))?;
+    text.parse::<I>()
+        .map_err(anyhow::Error::new)
+        .context("failed to parse input text")
+}
+
+pub fn _main<I, S1, S2>(path: &Path) -> Result<()>
+where
+    I: FromStr,
+    <I as FromStr>::Err: Error + Send + Sync + 'static,
+    S1: Solve<Input = I>,
+    S2: Solve<Input = I>,
+{
+    let input = _input(path)?;
+
+    let part_one = S1::solve(&input).context("failed to solve part 1")?;
+    println!("Part One: {}", part_one);
+
+    let part_two = S2::solve(&input).context("failed to solve part 2")?;
+    println!("Part Two: {}", part_two);
+
+    Ok(())
+}
