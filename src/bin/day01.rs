193a4d21c796939b2b1953f0827f378705adffe0
Max McCarthy <hello@maxmcc.com>
2020-11-30 21:54:12 -0800

    Add implementation for day 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,64 +1,60 @@
-// Note: this is day 1 from 2019, not 2020
-
 use aoc::{self, Result, Solve};
-use std::{num::ParseIntError, str::FromStr};
+use std::{collections::HashSet, num::ParseIntError, str::FromStr};
 
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-struct Masses(Vec<i32>);
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct ExpenseReport {
+    entries: HashSet<i32>,
+}
 
-impl FromStr for Masses {
+impl FromStr for ExpenseReport {
     type Err = ParseIntError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let masses = s.lines().map(str::parse).collect::<Result<_, _>>()?;
-        Ok(Masses(masses))
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let lines = input.lines().map(str::trim);
+        let entries = lines.map(str::parse).collect::<Result<_, _>>()?;
+        Ok(ExpenseReport { entries })
     }
 }
 
 struct PartOne;
 
-impl PartOne {
-    fn fuel_for_mass(mass: i32) -> i32 {
-        mass / 3 - 2
-    }
-}
-
 impl Solve for PartOne {
-    type Input = Masses;
+    type Input = ExpenseReport;
     type Solution = i32;
 
-    fn solve(input: &Masses) -> Result<Self::Solution> {
-        Ok(input.0.iter().copied().map(Self::fuel_for_mass).sum())
+    fn solve(input: &Self::Input) -> Result<Self::Solution> {
+        for x in input.entries.iter() {
+            let y = 2020 - x;
+            if input.entries.contains(&y) {
+                return Ok(x * y);
+            }
+        }
+        anyhow::bail!("no two numbers found");
     }
 }
 
 struct PartTwo;
 
-impl PartTwo {
-    fn fuel_for_module(module_mass: i32) -> i32 {
-        let mut remaining = module_mass;
-        let mut total = 0;
-        while remaining > 2 {
-            let fuel = PartOne::fuel_for_mass(remaining).max(0);
-            total += fuel;
-            remaining = fuel;
-        }
-        total
-    }
-}
-
 impl Solve for PartTwo {
-    type Input = Masses;
+    type Input = ExpenseReport;
     type Solution = i32;
 
-    fn solve(input: &Masses) -> Result<Self::Solution> {
-        Ok(input.0.iter().copied().map(Self::fuel_for_module).sum())
+    fn solve(input: &Self::Input) -> Result<Self::Solution> {
+        for x in input.entries.iter() {
+            for y in input.entries.iter() {
+                let z = 2020 - x - y;
+                if input.entries.contains(&z) {
+                    return Ok(x * y * z);
+                }
+            }
+        }
+        anyhow::bail!("no three numbers found");
     }
 }
 
 aoc::main!();
 
 aoc::solved! {
-    PartOne = 3262991,
-    PartTwo = 4891620,
+    PartOne = 1018944,
+    PartTwo = 8446464,
 }
