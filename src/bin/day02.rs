2671238fa3cc80ac2c08c6a08659637761431983
Max McCarthy <hello@maxmcc.com>
2020-12-02 00:21:24 -0800

    Use more Iterator methods

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,33 +1,32 @@
 use aoc::{self, Result, Solve};
 use scan_fmt::scan_fmt;
-use std::{collections::HashMap, str::FromStr};
+use std::str::FromStr;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
-struct PasswordEntry {
-    first_req: usize,
-    second_req: usize,
-    character: char,
-    password: String,
+struct Entry {
+    min: usize,
+    max: usize,
+    req: char,
+    pass: String,
 }
 
-impl FromStr for PasswordEntry {
+impl FromStr for Entry {
     type Err = anyhow::Error;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        let (first_req, second_req, character, password) =
-            scan_fmt!(input, "{}-{} {}: {}", usize, usize, char, String)?;
-        Ok(PasswordEntry {
-            first_req,
-            second_req,
-            character,
-            password,
+        let (min, max, req, pass) = scan_fmt!(input, "{}-{} {}: {}", _, _, _, _)?;
+        Ok(Entry {
+            min,
+            max,
+            req,
+            pass,
         })
     }
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 struct Passwords {
-    entries: Vec<PasswordEntry>,
+    entries: Vec<Entry>,
 }
 
 impl FromStr for Passwords {
@@ -51,16 +50,8 @@ impl Solve for PartOne {
             .entries
             .iter()
             .filter(|entry| {
-                let mut freqs = HashMap::new();
-                for ch in entry.password.chars() {
-                    let freq = freqs.entry(ch).or_insert(0);
-                    *freq += 1;
-                }
-                let range = entry.first_req..=entry.second_req;
-                freqs
-                    .get(&entry.character)
-                    .map(|freq| range.contains(&freq))
-                    .unwrap_or(false)
+                let count = entry.pass.chars().filter(|&it| it == entry.req).count();
+                count >= entry.min && count <= entry.max
             })
             .count())
     }
@@ -77,9 +68,9 @@ impl Solve for PartTwo {
             .entries
             .iter()
             .filter(|entry| {
-                let first = entry.password.chars().nth(entry.first_req - 1);
-                let second = entry.password.chars().nth(entry.second_req - 1);
-                (first == Some(entry.character)) ^ (second == Some(entry.character))
+                let x = entry.pass.chars().nth(entry.min - 1);
+                let y = entry.pass.chars().nth(entry.max - 1);
+                (x == Some(entry.req)) ^ (y == Some(entry.req))
             })
             .count())
     }
