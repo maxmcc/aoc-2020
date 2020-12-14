bd71fb8373cec879d01c8577f3f1e768961bb11e
Max McCarthy <hello@maxmcc.com>
2020-12-13 20:36:59 -0800

    Rename generic Input struct to BusSchedule

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -7,12 +7,12 @@ struct Bus {
     index: u64,
 }
 
-struct Input {
+struct BusSchedule {
     depart: u64,
     buses: Vec<Bus>,
 }
 
-impl<'a> Parse<'a> for Input {
+impl<'a> Parse<'a> for BusSchedule {
     fn parse<'b: 'a>(input_str: &'b str) -> Result<Self> {
         let mut lines = input_str.lines().map(str::trim);
         let depart = lines
@@ -31,14 +31,14 @@ impl<'a> Parse<'a> for Input {
                 Ok(Bus { headway, index })
             })
             .collect::<Result<Vec<_>>>()?;
-        Ok(Input { depart, buses })
+        Ok(BusSchedule { depart, buses })
     }
 }
 
 struct PartOne;
 
 impl Solve<'_> for PartOne {
-    type Input = Input;
+    type Input = BusSchedule;
     type Solution = u64;
 
     fn solve(input: &Self::Input) -> Result<Self::Solution> {
@@ -55,7 +55,7 @@ impl Solve<'_> for PartOne {
 struct PartTwo;
 
 impl<'a> Solve<'a> for PartTwo {
-    type Input = Input;
+    type Input = BusSchedule;
     type Solution = u64;
 
     fn solve(input: &Self::Input) -> Result<Self::Solution> {
@@ -80,7 +80,7 @@ mod examples {
 
     #[test]
     fn example_part_one() {
-        let input = Input::parse(indoc! {"
+        let input = BusSchedule::parse(indoc! {"
             939
             7,13,x,x,59,x,31,19
         "})
@@ -91,22 +91,22 @@ mod examples {
 
     #[test]
     fn example_part_two() {
-        let input = Input::parse("0\n7,13,x,x,59,x,31,19").unwrap();
+        let input = BusSchedule::parse("0\n7,13,x,x,59,x,31,19").unwrap();
         assert_eq!(PartTwo::solve(&input).unwrap(), 1068781);
 
-        let input = Input::parse("0\n17,x,13,19").unwrap();
+        let input = BusSchedule::parse("0\n17,x,13,19").unwrap();
         assert_eq!(PartTwo::solve(&input).unwrap(), 3417);
 
-        let input = Input::parse("0\n67,7,59,61").unwrap();
+        let input = BusSchedule::parse("0\n67,7,59,61").unwrap();
         assert_eq!(PartTwo::solve(&input).unwrap(), 754018);
 
-        let input = Input::parse("0\n67,x,7,59,61").unwrap();
+        let input = BusSchedule::parse("0\n67,x,7,59,61").unwrap();
         assert_eq!(PartTwo::solve(&input).unwrap(), 779210);
 
-        let input = Input::parse("0\n67,7,x,59,61").unwrap();
+        let input = BusSchedule::parse("0\n67,7,x,59,61").unwrap();
         assert_eq!(PartTwo::solve(&input).unwrap(), 1261476);
 
-        let input = Input::parse("0\n1789,37,47,1889").unwrap();
+        let input = BusSchedule::parse("0\n1789,37,47,1889").unwrap();
         assert_eq!(PartTwo::solve(&input).unwrap(), 1202161486);
     }
 }
